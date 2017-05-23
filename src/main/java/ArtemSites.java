9835e852891a45b58f1dbb72f27c16f7f609125d
SergeyOberemok <sergey.oberemok@gmail.com>
2017-05-23 15:43:45 +0300

    A csv files  were  deleted

--- src/main/java/ArtemSites.java
+++ src/main/java/ArtemSites.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+/*import GooglePages;
+import PingdomPages;*/
 
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
