30361136096e34a75389d5ac89ab768952fb02f2
Andrey Bzhestovskyy <bzhest@gmail.com>
2017-07-25 11:52:24 +0300

    temporarily

--- src/main/java/PingdomPages.java
+++ src/main/java/PingdomPages.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.swing.*;
 import java.util.List;
@@ -27,6 +29,7 @@ public class PingdomPages {
         this.mbValue = mbValue;
         this.secValue = secValue;
         this.url = url;
+
     }
 
     public PingdomPages(){}
@@ -68,7 +71,9 @@ public class PingdomPages {
     }
     public List <WebElement> getLinkTryAgain() {return linkTryAgain;}
     public WebElement getUrlInput() {return urlInput;}
-    public WebElement getPerformanceGrade() {return performanceGrade;}
+    public WebElement getPerformanceGrade() {
+        return performanceGrade;
+    }
     public WebElement getMb() {return mb;}
     public String getPerformGradeLetter() {
         return performGradeLetter;
